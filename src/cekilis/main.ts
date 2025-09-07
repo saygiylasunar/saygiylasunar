23fdce9e0ad56fbb0b9f5a61dac81ef4813d694b
Ersen Filiz <saygiylasunar@gmail.com>
2025-09-07 08:52:04 +0300

    🎲 Çekiliş

---
+++ src/cekilis/main.ts
@@ -0,0 +1,3 @@
+import { createApp } from 'vue';
+import Cekilis from './Cekilis.vue';
+createApp(Cekilis).mount('#app');
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
+
+// __dirname yerine process.cwd() kullanmak daha güvenli
+const r = (...paths) => resolve(process.cwd(), ...paths);
 
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: 5173
-  }
-})
+    port: 5173,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: r('index.html'),
+        cekilis: r('cekilis.html'),
+      },
+    },
+  },
+});
