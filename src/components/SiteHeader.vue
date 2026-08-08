b14528b8119d6ebec5d21400a4f00dd2eb21bab7
Ersen FİLİZ <146385365+saygiylasunar@users.noreply.github.com>
2026-08-08 09:18:45 +0300

    nav: add music section

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -27,6 +27,7 @@
       >
         <RouterLink to="/services">{{ t('nav.services') }}</RouterLink>
         <RouterLink to="/projects">{{ t('nav.projects') }}</RouterLink>
+        <RouterLink to="/music">{{ locale === 'tr' ? 'Müzik' : 'Music' }}</RouterLink>
         <RouterLink to="/experience">{{ t('nav.experience') }}</RouterLink>
         <RouterLink to="/about">{{ t('nav.about') }}</RouterLink>
         <RouterLink class="nav-contact" to="/contact">{{ t('nav.contact') }}</RouterLink>
