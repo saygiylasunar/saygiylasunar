2673ab93a8a3db155d3fc3d7b8c4d81f2e043a0c
Ersen FİLİZ <146385365+saygiylasunar@users.noreply.github.com>
2026-08-08 09:00:25 +0300

    feat: mount floating lands shortcut

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
     </main>
     <SiteFooter />
     <IntentModal />
+    <LandsFab />
     <ToolsFab />
   </div>
 </template>
@@ -20,6 +21,7 @@ import { RouterView, useRoute } from 'vue-router'
 import SiteHeader from './components/SiteHeader.vue'
 import SiteFooter from './components/SiteFooter.vue'
 import IntentModal from './components/IntentModal.vue'
+import LandsFab from './components/LandsFab.vue'
 import ToolsFab from './components/ToolsFab.vue'
 import { getProject, getService } from './lib/content.js'
 import { locale, localize, t } from './lib/locale.js'
