834052643e061c67147e7337ad50327bd2d3eedd
Ersen FİLİZ <146385365+saygiylasunar@users.noreply.github.com>
2026-08-08 09:03:43 +0300

    style: load purple accent palette

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ import './styles/lands-final.css'
 import './styles/lands-earth.css'
 import './styles/compact-scale.css'
 import './styles/lands-fab.css'
+import './styles/palette-purple.css'
 
 const app = createApp(App)
 const revealObservers = new WeakMap()
